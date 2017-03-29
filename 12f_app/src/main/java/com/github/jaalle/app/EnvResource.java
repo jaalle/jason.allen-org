734e914a87625cf5f927de083d626a8447dbef34
Jason Allen <jason.allen@sas.com>
2017-03-29 16:43:37 -0400

    Moved file "travis.yml".

--- 12f_app/src/main/java/com/github/jaalle/app/EnvResource.java
+++ 12f_app/src/main/java/com/github/jaalle/app/EnvResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class EnvResource {
 
   @Autowired
