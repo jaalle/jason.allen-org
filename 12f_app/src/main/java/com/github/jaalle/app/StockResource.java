bf3869a0a0336e55e8d216d068db99eab3cc579f
Jason Allen <jason.allen@sas.com>
2017-03-30 11:50:38 -0400

    201703301150

--- 12f_app/src/main/java/com/github/jaalle/app/StockResource.java
+++ 12f_app/src/main/java/com/github/jaalle/app/StockResource.java
@@ -2,6 +2,8 @@ package com.github.jaalle.app;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
@@ -55,18 +57,22 @@ public class StockResource {
 
   @Autowired
   private StockItemRepository stockItemRepository;
+  private static final Logger logger = LoggerFactory.getLogger(StockResource.class);
 
   @RequestMapping(method = RequestMethod.GET, value = "/{id}")
   public StockItem stockItem(@PathVariable("id") Long id, HttpServletResponse response) {
+      logger.info("Starting search of stock item(id={}) search", id);
 
-    StockItem stockItem = stockItemRepository.findOne(id);
+      StockItem stockItem = stockItemRepository.findOne(id);
 
-    if (stockItem == null) {
-      response.setStatus(HttpStatus.NOT_FOUND.value());
-      return null;
-    }
+      if (stockItem == null) {
+          logger.info("Stock item(id={}) has not been found", id);
+          response.setStatus(HttpStatus.NOT_FOUND.value());
+          return null;
+      }
 
-    return stockItem;
+      logger.info("Finishing search of stock item(id={}) search", id);
+      return stockItem;
   }
 
   @RequestMapping(value = "", method = RequestMethod.POST)
